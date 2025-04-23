06d2d75fc3f992299b79c187db9c1032d0a0bcdf
Przemus14s <przemekszynal5@gmail.com>
2025-04-23 17:05:08 +0200

    initial commit

---
+++ src/main/java/com/example/scrap_app/dto/ScrapDto.java
@@ -0,0 +1,32 @@
+package com.example.scrap_app.dto;
+
+public class ScrapDto {
+
+    private String title;
+    private String source;
+    private String link;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
